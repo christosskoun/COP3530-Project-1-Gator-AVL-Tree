025093a16d4195d4aebff1b027e2766c4c7a6c63
christosskoun <christosskoun@gmail.com>
2023-06-11 23:11:04 -0400

    Created ouline for the main function of "main.cpp" and the function definitions of the rotation helper functions

--- src/AVL_Tree.h
+++ src/AVL_Tree.h
@@ -7,6 +7,13 @@ struct TreeNode{
     TreeNode* left;
     TreeNode* right;
     TreeNode(int x): value(x), left(nullptr), right(nullptr) {}
+
+    //Helper functions to keep tree Balanced (AKA rotation functions)
+    TreeNode * rotateLeft (TreeNode* node); //O(1)
+    TreeNode *  rotateRight (TreeNode* node); //O(1)
+    TreeNode *  rotateLR (TreeNode* node); //O(1)
+    TreeNode *  rotateRL (TreeNode* node); //O(1)
+
     void insert(std::string name, int ID);
     void remove(int ID);
     void search(int ID); //searches for a Node with a specific ID
@@ -19,7 +26,39 @@ struct TreeNode{
 
 };
 
+
+TreeNode * TreeNode::rotateLeft(TreeNode *node) {
+    TreeNode* grandchild=node->right->right;//was originally right->left from slides
+    TreeNode* newParent=node->right;
+    newParent->left=node;
+    node->right=grandchild;
+    return newParent;
+}
+
+TreeNode * TreeNode::rotateRight(TreeNode *node) {
+    TreeNode* grandchild=node->left->left;
+    TreeNode* newParent=node->left;
+    newParent->left=grandchild;
+    node->right=node;
+    return newParent;
+}
+
+TreeNode * TreeNode::rotateLR(TreeNode *node) {
+    rotateLR(node); //TODO pass in parameter nodes for rotations by reference?
+    rotateRight(node);
+}
+
+TreeNode * TreeNode::rotateRL(TreeNode *node) {
+    rotateRight(node);
+    rotateLR(node);
+}
+
+
+
 void TreeNode::insert(std::string name, int ID) {
+    //search for location to put Node in
+    //actually put node in
+    //if/else statements to take care of rotations
 
 }
 
@@ -54,3 +93,4 @@ void TreeNode::printLevelCount() {
 void TreeNode::removeInorder(unsigned int n) {
 
 }
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 
 /* Note: 
 	1. You will have to comment main() when unit testing your code because catch uses its own main().
@@ -6,6 +7,67 @@
 */
 
 int main(){
+    short operationCount;
+    std::cin>>operationCount;
+
+    while(operationCount>=0) {
+
+        //reading one line of input from cin
+        std::string currLine;
+        getline(std::cin, currLine);
+
+        //create an input stream from that line
+        std::istringstream lineStream(currLine);
+
+        //read the first word from the input stream
+        std::string command;
+        lineStream >> command;
+
+        if (command=="insert"){
+
+        }
+
+        else if (command=="remove"){
+
+        }
+
+        else if (command=="search"){
+
+        }
+
+        else if (command=="printInorder"){
+
+        }
+
+        else if (command=="printPreorder"){
+
+        }
+
+        else if (command=="printPostorder"){
+
+        }
+
+        else if (command=="printLevelCount"){
+
+        }
+
+        else if (command=="removeInorder"){
+
+        }
+
+
+
+        operationCount--;//decrement operationCount as we just completed 1 operation
+    }
+
 	return 0;
 }
+
+
+
+/*
+        //read up to the next quotation mark
+        std::string next;
+        getline(lineStream, next, '"');
+*/
 
